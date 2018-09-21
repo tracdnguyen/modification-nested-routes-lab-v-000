4f505b8e32fdeee462d54a642d1f1a84336f6181
Trac Nguyen <nguyendtrac@gmail.com>
2018-09-21 00:20:39 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,7 +3,10 @@ module ArtistsHelper
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
 
-  def artist_select()
-
+  def artist_select(artist, song)
+    if artist
+      artist.name
+    else
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
   end
 end
